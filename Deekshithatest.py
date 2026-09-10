6b311829acb7efde762ad78f868861afa01035ac
Anirudh <anirudh11042005@gmail.com>
2026-09-10 17:20:16 +0530

    I corrected Deekshitha's code

--- Deekshithatest.py
+++ Deekshithatest.py
@@ -1,6 +1,13 @@
 #10/09/2026
- str1=" JESUS CHRIST MY FATHER Deekshitha.A Soloman tution centre Drama team "
+#   str1=" JESUS CHRIST MY FATHER Deekshitha.A Soloman tution centre Drama team "
+# print(str1[1:22])
+# print(str1[23:36])
+# print(str1[-11:-1])
+# print(str1[-34:-12]) 
+
+# The mistake in your code is
+str1=" JESUS CHRIST MY FATHER Deekshitha.A Soloman tution centre Drama team "
 print(str1[1:22])
 print(str1[23:36])
 print(str1[-11:-1])
-print(str1[-34:-12]) 
+print(str1[-34:-12])
