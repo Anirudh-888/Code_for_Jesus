4ca10a1e6da64dbae624f6fd19397093629eef75
sannsanju <sanjanasr77@gmail.com>
2026-09-11 16:58:48 +0530

    Update Sanjana test.py

--- Sanjana test.py
+++ Sanjana test.py
@@ -3,3 +3,14 @@ print(str[0:5])
 print(str[1:4])
 print(str[2:3])
 #The date of the test is 10/09/2026
+
+str="sAnJaNa"
+capital=str.upper()
+title=capital.title()
+lower=title.lower()
+print(str)
+print(capital)
+print(title)
+print(lower)
+#stringfunctions
+#The date of the test is 11/09/2026
